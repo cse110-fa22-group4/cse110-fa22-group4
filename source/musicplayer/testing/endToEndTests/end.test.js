31e37b452b63bbda1430cd6c72c168e6f1111f0e
Jeremy Lei <jeremy.lei.2002@gmail.com>
2022-11-29 20:17:56 -0800

    attempt at testing ffMeta and e2e

---
+++ source/musicplayer/testing/endToEndTests/end.test.js
@@ -0,0 +1,24 @@
+const { _electron: electron } = require('playwright')
+const { test, expect } = require('@playwright/test')
+
+const {
+  reset_user1, reset_user2, reset_stats, reset_user_blank
+} = require('../fsAPITesting/fsAPITester');
+const { setStoragePath } = require('../../preload/fs/fsAPICalls');
+const {
+    getStats, writeToStat, deleteStat, writeStats,
+  } = require('../../preload/fs/stats/statsAPICalls');
+const { BrowserWindow } = require('electron');
+
+let electronApp;
+
+test('load', async () => {
+  electronApp = await electron.launch({ args: ['main/main.js'] })
+  
+  const window = await electronApp.firstWindow();
+  
+  console.log(window);
+
+  await electronApp.close()
+
+});
