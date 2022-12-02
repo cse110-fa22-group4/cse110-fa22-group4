fd8d95e684993b2db0245223aadeab4d163fd451
Pikachu <xuh013@ucsd.edu>
2022-12-02 14:03:50 -0800

    Updated playlist test

--- source/musicplayer/testing/fsAPITests/playlistAPI.test.js
+++ source/musicplayer/testing/fsAPITests/playlistAPI.test.js
@@ -5,16 +5,16 @@ const functions_playlist = require('../../preload/fs/playlists/playlistAPICalls'
 const {fsInit, getStoragePath, setStoragePath, makeDirIfNotExists} = require("../../preload/fs/fsAPICalls");
 const path = require("path");
 const fs = require("fs");
-const {removePlaylist, writePlaylist, createPlaylist, getPlaylist, writeToPlaylist, getPlaylistMeta, getPlaylistObj} = require("../../preload/fs/playlists/playlistAPICalls");
+const {removePlaylist, writePlaylist, createPlaylist, getPlaylist, writeToPlaylist, getPlaylistMeta, getPlaylistObj,
+    removeFromPlaylist, writePlaylistMeta, removePlaylistMeta
+} = require("../../preload/fs/playlists/playlistAPICalls");
 const fsPromises = fs.promises;
 
-let electronApp;
 
 /**
  * Set up the initial stage for the test
  */
 test.beforeAll(async () => {
-    //electronApp = electron.launch({ args:['/main/main.js'] });
 
     const testPath = '../source/users/user_3/songs';
     // Read an empty file without any playlist in it.
@@ -73,37 +73,111 @@ test('Checking getPlaylist() after adding a playlist', async () => {
 });
 
 /**
- * Test write Metadata to the playlist
+ * Test write Tags to the playlist
  */
 test('Testing writeToPlaylist(playlistName, tagGroup)', async () => {
     const tags = {};
     tags['artist'] = 'unknown';
     tags['title'] = 'playlistTest';
     tags['year'] = '2022';
+    // Write the above tags to the playlist
     await writeToPlaylist('New Playlist', tags);
-    const playlist_Meta = await getPlaylistMeta('New Playlist');
-    console.log(playlist_Meta);
 
+    // Get the data from the playlist
     const obj_playlist = await getPlaylistObj('New Playlist');
-    console.log(obj_playlist);
+    const obj_tags = obj_playlist.tags;
+
+    const str_tags = JSON.stringify(obj_tags[0]);
+    expect(str_tags).toBe("{\"artist\":\"unknown\",\"title\":\"playlistTest\",\"year\":\"2022\"}");
+});
+
+/**
+ * Test Delete Tags from the playlist
+ */
+test('Testing removeFromPlaylist(playlistName, index)', async () => {
+
+    // write an additional tags to the playlist
+    const tags = {};
+    tags['artist2'] = 'unknown';
+    tags['duration'] = '16 mins';
+    tags['album'] = 'Team4';
+
+    // write an additional tags to the playlist
+    const tags1 = {};
+    tags1['artist3'] = 'unknown';
+    tags1['date'] = '2022-11-11';
+
+    // Write the above tags to the playlist
+    await writeToPlaylist('New Playlist', tags);
+    await writeToPlaylist('New Playlist', tags1);
+
+    let obj_playlist = await getPlaylistObj('New Playlist');
+    const obj_tags = obj_playlist.tags;
+
+    let str_tags = JSON.stringify(obj_tags);
+
+    // check if new tags being added
+    expect(str_tags).toBe("[{\"artist\":\"unknown\",\"title\":\"playlistTest\",\"year\":\"2022\"}," +
+        "{\"artist2\":\"unknown\",\"duration\":\"16 mins\",\"album\":\"Team4\"}," +
+        "{\"artist3\":\"unknown\",\"date\":\"2022-11-11\"}]");
+
+    // delete the second tags object
+    await removeFromPlaylist('New Playlist', 1);
+    obj_playlist = await getPlaylistObj('New Playlist');
+    str_tags = JSON.stringify(obj_playlist.tags);
+    expect(str_tags).toBe("[{\"artist\":\"unknown\",\"title\":\"playlistTest\",\"year\":\"2022\"},{\"artist3\":\"unknown\",\"date\":\"2022-11-11\"}]");
+
+    // delete the last tags object
+    await removeFromPlaylist('New Playlist', 1);
+    obj_playlist = await getPlaylistObj('New Playlist');
+    str_tags = JSON.stringify(obj_playlist.tags);
+    expect(str_tags).toBe("[{\"artist\":\"unknown\",\"title\":\"playlistTest\",\"year\":\"2022\"}]");
+
+
+    // delete the rest of the tags object
+    await removeFromPlaylist('New Playlist', 0);
+    obj_playlist = await getPlaylistObj('New Playlist');
+    str_tags = JSON.stringify(obj_playlist.tags);
+    expect(str_tags).toBe("[]");
 });
 
 
+/**
+ * Test write/get/remove Meta to the playlist.
+ */
+test('Testing write/get/remove Metadata to the playlist', async () =>{
 
-// /**
-//  * Test functions together
-//  */
-// test.describe('Checking writePlaylist and removing playlist', () => {
-//     test('write a playlist', async () => {
-//         await writePlaylist('Testing', )
-//     });
-// });
+    // testing write and get Meta
+    await writePlaylistMeta('New Playlist', 'creator', 'team4');
+    let playlist_Meta = await getPlaylistMeta('New Playlist');
+    expect(JSON.stringify(playlist_Meta)).toBe("{\"creator\":\"team4\"}");
+
+    // write an additional meta to the playlist
+    await writePlaylistMeta('New Playlist', 'created',"2022-11-11");
+    playlist_Meta = await getPlaylistMeta('New Playlist');
+    expect(JSON.stringify(playlist_Meta)).toBe("{\"creator\":\"team4\",\"created\":\"2022-11-11\"}");
+
+    // delete one metadata from the playlist
+    await removePlaylistMeta('New Playlist', 'creator');
+    playlist_Meta = await getPlaylistMeta('New Playlist');
+    expect(JSON.stringify(playlist_Meta)).toBe("{\"created\":\"2022-11-11\"}");
+
+    // delete a non-existing metadata from the playlist
+    await removePlaylistMeta('New Playlist', 'artist');
+    playlist_Meta = await getPlaylistMeta('New Playlist');
+    expect(JSON.stringify(playlist_Meta)).toBe("{\"created\":\"2022-11-11\"}");
+
+    // delete the rest metadata from the playlist
+    await removePlaylistMeta('New Playlist', 'created');
+    playlist_Meta = await getPlaylistMeta('New Playlist');
+    expect(JSON.stringify(playlist_Meta)).toBe("{}");
+});
+
 
 /**
  * Close the app after the all the tests ends
  */
 test.afterAll(async () =>{
-    //electronApp.close();
     // Empty the all the files in testing playlist
     const playlistPath = path.join(await getStoragePath(), 'playlists');
     for (const file of await fsPromises.readdir(playlistPath)) {
