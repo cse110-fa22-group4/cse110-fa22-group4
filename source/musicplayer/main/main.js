4da2f6fd266748250c052b2e63d5f1df66d5296e
Liam <lgolly@ucsd.edu>
2022-12-02 19:55:50 -0800

    unregistered refresh button min main.js line 72-75

--- source/musicplayer/main/main.js
+++ source/musicplayer/main/main.js
@@ -26,8 +26,6 @@ const createWindow = async () => {
 
 	mainWindow.on('focus', () => {
 		mainWindow.webContents.send('window-focused');
-		globalShortcut.unregister('CommandOrControl+R');
-		globalShortcut.unregister('CommandOrControl+Shift+R');
 	});
 
 	mainWindow.on('blur', () => {
@@ -71,8 +69,13 @@ app.whenReady().then(async () => {
 		if (BrowserWindow.getAllWindows().length === 0) createWindow();
 	});
 
+	await globalShortcut.unregister('CmdOrCtrl+R');
+	await globalShortcut.unregister('CmdOrCtrl+Shift+R');
+	globalShortcut.register('CmdOrCtrl+R', () => { });
+	globalShortcut.register('CmdOrCtrl+Shift+R', () => { });
 	//
 });
+
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
