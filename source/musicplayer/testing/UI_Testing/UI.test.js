f9c22a304479e7307fb982acd37c998ff6972830
Jeremy Lei <jeremy.lei.2002@gmail.com>
2022-12-04 22:10:52 -0800

    updated UI test (used for recording)

--- source/musicplayer/testing/UI_Testing/UI.test.js
+++ source/musicplayer/testing/UI_Testing/UI.test.js
@@ -5,6 +5,7 @@ const fun_metaData = require('../../preload/ffmpeg/metadata/ffMetaAPICalls');
 const { BrowserWindow } = require('electron');
 const { getAttribute, loadPage} = require('../../preload/dom/domAPICalls');
 const { setStoragePath } = require('../../preload/fs/fsAPICalls');
+const { removePlaylist } = require('../../preload/fs/playlists/playlistAPICalls');
 
 let electronApp;
 let window;
@@ -21,25 +22,11 @@ async function getContents(query) {
 
 /**
  * 
+ * @returns 
  */
-test('Start', async () => {
-    const electronApp = await electron.launch({args: ['main/main.js']});
-    const appPath = await electronApp.evaluate(async ({app}) => {
-        return app.getAppPath();
-    });
-    //console.log(appPath);
-    window = await electronApp.firstWindow();
-    //console.log(await window.title());
-
-    // Show the console in electron in the current console
-    //await window.on('console', console.log);
-    //console.log(window);
-    //await window.pause();
-
-    
-
-    await electronApp.close()
-});
+function sleep(time=500) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
 
 /**
  * 
@@ -50,158 +37,249 @@ test('Check Page Navigation', async () => {
         return app.getAppPath();
     });
     window = await electronApp.firstWindow();
+    test.setTimeout(120000);
 
-    // allow for 5 minutes for debugging
-    test.setTimeout(300000);
+    await window.pause();
+
+    // reset environment
+    await removePlaylist('some-playlist-name');
+    await removePlaylist('another-playlist-name');
+    await removePlaylist('bad-playlist');
 
     // Navigate to Library
     await window.locator('#btn-library').click();
-    expect(await getContents('#header-title')).toStrictEqual(
-        ["Library"]);
-    //console.log(await library.allInnerTexts());
+    await sleep();
 
     // Navigate to Playlists
     await window.locator('#btn-playlists').click();
-    expect(await getContents('#header-title')).toStrictEqual(
-        ["Playlists"]);
+    await sleep();
 
     // Navigate to Home
     await window.locator('#btn-home').click();
-    expect(await getContents('#header-title')).toStrictEqual(
-        ["Home"]);
+    await sleep();
+    
 
     // Navigate to Settings
     await window.locator('#btn-settings').click();
-    expect(await window.locator('#top-container-extended').getAttribute('style')).toStrictEqual("visibility: visible;");
-
+    await sleep();
+    
     // Close Settings
     await window.locator('#btn-settings').click();
+    await sleep();
 
     // Navigate to Library
     await window.locator('#btn-library').click();
+    await sleep();
 
     // lower volume to 50%
     await window.locator('#audio-fader').click();
-
-    //await window.pause();
+    await sleep();
 
     // add third song in library to queue
     await window.locator('#library-container :text("+")').nth(2).click();
+    await sleep();
 
     // add first song in library to queue
     await window.locator('#library-container :text("+")').nth(0).click();
+    await sleep();
 
     // add second song in library to queue
     await window.locator('#library-container :text("+")').nth(1).click();
+    await sleep();
 
-    // click shuffle (for some reason needs to be clicked twice)
+    // click shuffle twice
     await window.locator('#shuffle-btn').click();
+    await sleep();
     await window.locator('#shuffle-btn').click();
+    await sleep();
 
     //open queue
     await window.locator('#playlists-bottom-btn').click();
+    await sleep();
 
     // Click play
     await window.locator('#play-btn').click();
+    await sleep(5000);
 
 
     // Click next track
     await window.locator('#next-btn').click();
+    await sleep(2000);
     
     // Click prev track
     await window.locator('#prev-btn').click();
+    await sleep(2000);
 
     // Delete second track in queue
     await window.locator('.btn-queue-track-delete').nth(1).click();
+    await sleep();
 
     // Click next track
     await window.locator('#next-btn').click();
-
-    // Click loop (causes problems rn)
-    //await window.locator('#loop-btn').click();
+    await sleep(1000);
     
     // add first song in library to queue
     await window.locator('#library-container :text("+")').nth(0).click();
+    await sleep(1000);
 
     // clear queue
     await window.locator('#btn-queue-clear').click();
+    await sleep();
 
     // select all songs to be added to queue
     await window.locator('.gridjs-checkbox').nth(0).click();
     await window.locator('.gridjs-checkbox').nth(1).click();
     await window.locator('.gridjs-checkbox').nth(2).click();
+    await sleep();
 
     // add all selected items to queue
     await window.locator('#btn-addQueue').click();
-
-    //await window.pause();
+    await sleep();
 
     //open playlist popup
     await window.locator('#btn-playlist').click();
+    await sleep();
 
     // type in textbox
     await window.locator('#input-playlist-create').type("some-playlist-name");
+    await sleep();
     
     // create playlist
     await window.locator('#btn-playlist-create').click();
+    await sleep();
 
     // select all songs to be added to playlist
     await window.locator('.gridjs-checkbox').nth(0).click();
     await window.locator('.gridjs-checkbox').nth(1).click();
     await window.locator('.gridjs-checkbox').nth(2).click();
+    await sleep();
 
     // Add selected items to playlist
     await window.locator('#btn-playlist-add').click();
+    await sleep();
 
     // Go to Playlist page
     await window.locator('#btn-playlists').click();
+    await sleep();
 
-    //await window.pause();
 
     // click on playlist
     await window.locator('.library-card-info :text("some-playlist-name")').click();
+    await sleep();
 
-    //await window.pause();
-
-    window.pause();
 
     // play from playlist
     await window.locator('#btn-playlist-playAll').click();
+    await sleep();
+
+    // Click play
+    await window.locator('#play-btn').click();
+    await sleep(2000);
     
     // create more playlists
     await window.locator('#input-playlist-create').type("another-playlist-name");
+    await sleep();
     await window.locator('#btn-playlist-create').click();
+    await sleep();
 
+
+    // Add songs to playlist
+    await window.locator('.gridjs-checkbox').nth(0).click();
+    await sleep();
+    await window.locator('#btn-playlist-add').click();
+    await sleep();
+
+    await window.locator('.gridjs-checkbox').nth(1).click();
+    await sleep();
+    await window.locator('#btn-playlist-add').click();
+    await sleep();
+
+    await window.locator('.gridjs-checkbox').nth(2).click();
+    await sleep();
+    await window.locator('#btn-playlist-add').click();
+    await sleep();
+
+    // create another playlist
     await window.locator('#input-playlist-create').type("bad-playlist");
+    await sleep();
     await window.locator('#btn-playlist-create').click();
+    await sleep();
 
-    // click on drop down for playlist selection
-    await window.locator('#select-playlist-add').click()
+    // delete it
+    await window.locator('#btn-playlist-delete').click();
+    await sleep();
 
-    // select playlist to modify
-    //await window.locator('#playlist-option-another-playlist-name').click()
+    //open queue
+    await window.locator('#playlists-bottom-btn').click();
+    await sleep();
+    
+    // Go to Playlist page
+    await window.locator('#btn-playlists').click();
+    await sleep();
 
-    //go to home
-    //await window.locator('#btn-home').click();
+    // click on playlist
+    await window.locator('.library-card-info :text("another-playlist-name")').click();
+    await sleep();
+    
+    // click shuffle button
+    await window.locator('#shuffle-btn').click();
+    await sleep();
 
-    // click on Tobu
-    //await window.locator('.library-card-info :text("Tobu")').click();
-    //library-card library-card-artist
-    //data-libtarget="Tobu"
+    // click playlist play
+    await window.locator('#btn-playlist-playAll').click();
+    await sleep();
 
+    // Click play
+    await window.locator('#play-btn').click();
+    await sleep(5000);
 
-    // '#button-queue-clear'
-    // '.gridjs-checkbox'
-    // '#btn-addQueue'
-    // '#btn-playlist'
+    // Click next track 3 times
+    await window.locator('#next-btn').click();
+    await sleep(2000);
+    await window.locator('#next-btn').click();
+    await sleep(2000);
+    await window.locator('#next-btn').click();
+    await sleep(2000);
 
-    window.pause();
+    // click prev button 2 times
+    await window.locator('#prev-btn').click();
+    await sleep(2000);
+    await window.locator('#prev-btn').click();
+    await sleep(2000);
 
-   //await window.pause();
+    // turn off shuffle
+    await window.locator('#shuffle-btn').click();
+    await sleep();
+
+    // click loop button
+    await window.locator('#loop-btn').click();
+    await sleep();
+
+    // click playlist play
+    await window.locator('#btn-playlist-playAll').click();
+    await sleep();
+
+    // Click play
+    await window.locator('#play-btn').click();
+    await sleep(3000);
+
+    // Click next track twice
+    await window.locator('#next-btn').click();
+    await sleep(3000);
+    await window.locator('#next-btn').click();
+    await sleep(3000);
+
+    // clear queue
+    await window.locator('#btn-queue-clear').click();
+    await sleep(2000);
+
+    await window.pause();
+
    await electronApp.close();
 
 });
 
+//await new Promise(r => setTimeout(r, 2000));
 
 /*
 Next Steps:
