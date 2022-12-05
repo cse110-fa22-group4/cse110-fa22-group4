f5c16d814b03571eb1b243a089c72ebd56660d45
RealHumanBeen <amangaliman@gmail.com>
2022-12-04 21:59:58 -0800

    updated play/pause and volume icons to be compatible with dark mode

--- source/musicplayer/renderer/components/playback.js
+++ source/musicplayer/renderer/components/playback.js
@@ -280,12 +280,15 @@ function toggleColor(toggle, btn) {
  */
 function toggleIcon() {
 	let btn = document.querySelector('.playbackBtn:nth-of-type(3)');
-	let btnImg = btn.querySelector('img');
+	let btnIcon = document.querySelector('#play-btn-icon');
+	// let btnImg = btn.querySelector('img');
 	if (btn.id === 'play-btn') {
-		btnImg.src = '../img/icons/playback/pause.png';
+		// btnImg.src = '../img/icons/playback/pause.png';
+        btnIcon.innerHTML = '<path d="M26.25 38V10H38v28ZM10 38V10h11.75v28Z"/>';
 		(btn).id = 'pause-btn';
 	} else {
-		btnImg.src = '../img/icons/playback/play.png';
+		// btnImg.src = '../img/icons/playback/play.png';
+        btnIcon.innerHTML = '<path d="M16 37.85v-28l22 14Z"/>';
 		(btn).id = 'play-btn';
 	}
 }
@@ -301,9 +304,9 @@ function updateVolumeIcon() {
 	// console.log(audioIcon);
 	console.log(audioFader.value);
 	if (audioFader.value === '0') {
-		audioIcon.src = '../img/icons/playback/muted.png';
+		audioIcon.innerHTML = '<path d="m40.65 45.2-6.6-6.6q-1.4 1-3.025 1.725-1.625.725-3.375 1.125v-3.1q1.15-.35 2.225-.775 1.075-.425 2.025-1.125l-8.25-8.3V40l-10-10h-8V18h7.8l-11-11L4.6 4.85 42.8 43Zm-1.8-11.6-2.15-2.15q1-1.7 1.475-3.6.475-1.9.475-3.9 0-5.15-3-9.225-3-4.075-8-5.175v-3.1q6.2 1.4 10.1 6.275 3.9 4.875 3.9 11.225 0 2.55-.7 5t-2.1 4.65Zm-6.7-6.7-4.5-4.5v-6.5Q30 17 31.325 19.2q1.325 2.2 1.325 4.8 0 .75-.125 1.475-.125.725-.375 1.425Zm-8.5-8.5-5.2-5.2 5.2-5.2Z"/>';
 	} else {
-		audioIcon.src = '../img/icons/playback/unmuted.png';
+		audioIcon.innerHTML = '<path d="M28 41.45v-3.1q4.85-1.4 7.925-5.375T39 23.95q0-5.05-3.05-9.05-3.05-4-7.95-5.35v-3.1q6.2 1.4 10.1 6.275Q42 17.6 42 23.95t-3.9 11.225Q34.2 40.05 28 41.45ZM6 30V18h8L24 8v32L14 30Zm21 2.4V15.55q2.75.85 4.375 3.2T33 24q0 2.85-1.65 5.2T27 32.4Z"/>';
 	}
 	// might want to add a separate triggered in change
 	// ffmpegAPI.changeVolume(audioFader.value);
